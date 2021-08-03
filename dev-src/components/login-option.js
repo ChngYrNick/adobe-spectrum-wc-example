79fb377858c1e6554e3f427acd9645765a1970e8
Serhii Moskovko <cakedispensers@gmail.com>
2021-08-03 15:24:21 +0300

    Create universal login option component

---
+++ dev-src/components/login-option.js
@@ -0,0 +1,67 @@
+import {LitElement, html, css} from 'lit';
+
+import {firebase} from '../services/firebase.js';
+import {localStorageSvc} from '../services/localStorage.js';
+
+export class LoginOption extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: flex;
+        border-top: solid 1px gray;
+        align-items: center;
+      }
+
+      :host > .icon {
+        border-right: solid 1px gray;
+        width: 35px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      :host > a {
+        padding-left: 35px;
+        font-style: italic;
+        text-decoration: none;
+      }
+
+      :host > a:hover {
+        text-decoration: underline;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      title: {type: String},
+      icon: {type: String},
+      provider: {type: Object},
+    };
+  }
+
+  render() {
+    return html`
+      <div class="icon">${this.icon}</div>
+      <a href="" @click=${this._onClick}>Login With ${this.title}</a>
+    `;
+  }
+
+  _onClick(e) {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithPopup(this.provider)
+      .then((result) => {
+        const {providerId, accessToken} = result.credential;
+        localStorageSvc.setToken(providerId, accessToken);
+        window.location.replace('https://localhost:8000/dev/');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
+
+window.customElements.define('login-option', LoginOption);
--- dev-src/components/page/sign-in.js
+++ dev-src/components/page/sign-in.js
@@ -1,13 +1,45 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
+import {facebookProvider, googleProvider} from '../../services/firebase.js';
 
-import '../facebook-login.js';
-import '../google-login.js';
+import '../login-option.js';
 
 export class SignIn extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        border: solid 1px gray;
+        max-width: 350px;
+        margin: 100px auto 0;
+        font-size: 1.2em;
+      }
+
+      :host > .title {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      :host > * {
+        height: 35px;
+      }
+    `;
+  }
   render() {
     return html`
-      <facebook-login></facebook-login>
-      <google-login></google-login>
+      <div class="title">
+        <p1>Sing In</p1>
+      </div>
+      <login-option
+        .icon=${''}
+        .title=${'Google'}
+        .provider=${googleProvider}
+      ></login-option>
+      <login-option
+        .icon=${''}
+        .title=${'Facebook'}
+        .provider=${facebookProvider}
+      ></login-option>
     `;
   }
 }
